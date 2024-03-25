ab77895a92f0307702f1786ea90f27f8c1f54032
Shokhruz <freefireuzbek12@gmail.com>
2024-03-25 12:18:54 +0500

    added space to all articles

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -19,7 +19,7 @@ export default function ResourcesPage() {
         setAllArticles(articles)
     }
     return (
-        <div className='custom-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[20px] md:gap-x-[30px] md:gap-y-[30px]'>
+        <div className='custom-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[20px] md:gap-x-[30px] md:gap-y-[30px] pb-[23px]'>
             {
                 allArticles.map((article, index) => (
                     <Link href={{
