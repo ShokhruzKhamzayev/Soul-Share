393d8c7e75096bf160050c4e8c571c8183b98178
Shohruz <freefireuzbek12@gmail.com>
2025-07-06 12:32:10 +0500

    added remote patterns rule to next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'media.graphassets.com'
+                hostname: 'us-east-1-shared-usea1-02.graphassets.com'
             }
         ]
     }
